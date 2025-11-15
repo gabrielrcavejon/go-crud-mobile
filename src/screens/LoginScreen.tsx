a6ffa2f4eb570285c4b3220a1143b2fe6d5e6a18
gabrielrcavejon <gabriel.cavejon@hotmail.com>
2025-11-15 15:35:08 -0300

    feat(login): melhorar layout da tela de login

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,5 +1,13 @@
 import { useState } from "react";
-import { Alert, Button, StyleSheet, TextInput, View } from "react-native";
+import {
+  ActivityIndicator,
+  Alert,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import { useAuth } from "../context/AuthContext";
 
 export default function LoginScreen() {
@@ -34,32 +42,97 @@ export default function LoginScreen() {
   }
 
   return (
-    <View>
+    <View style={styles.container}>
+      <Text style={styles.title}>Bem-vindo!</Text>
+      <Text style={styles.subtitle}>
+        Acesse sua conta no Go Crud Mobile para ver seus produtos!
+      </Text>
+
       <TextInput
         placeholder="Email"
+        placeholderTextColor="#888"
         value={email}
+        keyboardType="email-address"
         onChangeText={setEmail}
         autoCapitalize="none"
         style={styles.input}
+        editable={!loading}
       />
 
       <TextInput
         placeholder="Senha"
+        placeholderTextColor="#888"
         value={senha}
         onChangeText={setSenha}
+        secureTextEntry
         autoCapitalize="none"
         style={styles.input}
+        editable={!loading}
       />
 
-      <Button title="Entrar" onPress={handleLogin} disabled={loading} />
+      <TouchableOpacity
+        style={[styles.button, loading && styles.buttonDisabled]} // Aplica estilo de disabled se estiver carregando
+        onPress={handleLogin}
+        disabled={loading}
+      >
+        {loading ? (
+          <ActivityIndicator color="#fff" />
+        ) : (
+          <Text style={styles.buttonText}>Entrar</Text>
+        )}
+      </TouchableOpacity>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1, // Ocupa a tela inteira
+    justifyContent: "center", // Centraliza verticalmente
+    alignItems: "center", // Centraliza horizontalmente
+    backgroundColor: "#f4f4f8", // Fundo levemente cinza
+    padding: 30,
+  },
   input: {
+    width: "100%", // Pega todo o container, mas nao fica super esticado por conta do padding 30 ali do container
     borderWidth: 1,
+    height: 50,
+    borderRadius: 8,
+    fontSize: 16,
+    paddingHorizontal: 15,
+    borderColor: "#c9c3c3ff", // Borda sutil
+    marginBottom: 15, // Espaço entre os inputs
+  },
+  title: {
+    fontSize: 32,
+    textAlign: "center",
+    fontWeight: "bold",
+    color: "#333",
+    marginBottom: 5,
+  },
+  subtitle: {
+    textAlign: "center",
+    fontSize: 16,
+    color: "#666",
+    marginBottom: 40,
+  },
+
+  button: {
+    width: "100%",
+    height: 50,
+    backgroundColor: "#0b7bf2ff",
+    borderRadius: 8,
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 10,
-    padding: 10,
+    marginBottom: 20,
+  },
+  buttonDisabled: {
+    backgroundColor: "#a0c4ff", // Azul mais claro quando desabilitado
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
   },
 });
